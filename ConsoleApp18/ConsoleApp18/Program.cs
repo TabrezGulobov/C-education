f8d1d89b2b741a231044c859712f6563624527f1
TabrezGulobov <tabrezgulobov@gmail.com>
2024-11-30 10:34:23 +0500

    Add files via upload

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp7
+
+{
+    class Pogram
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            while (a >= b)
+            {
+                a -= b;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
