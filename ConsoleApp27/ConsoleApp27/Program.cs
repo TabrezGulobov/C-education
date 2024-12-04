15569def8e297e890ed8ed12481b96244dc9d434
TabrezGulobov <tabrezgulobov@gmail.com>
2024-12-04 17:54:47 +0500

    Add files via upload

---
+++ ConsoleApp27/ConsoleApp27/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp27
+
+{
+    class Pogram
+    {
+        static void Main(string[] args)
+        {
+            // int 1f = 1;
+            // int 2f = 1 * 2;
+            // int 3f= 1 * 2 * 3;
+            // int 4f = 1 * 2 * 4 ;
+            // int n = 1 * 2 * 3...n ;
+            //
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+           
+            int i = 1;
+            while (n >= 1)
+            {
+                i *= n;
+                n -= 2;
+            }
+            Console.WriteLine($"Двойное фактериальное решение {i}");
+        }
+    }
+}
